63b16875f0ec7e729d3572ac2c9ee475ae2bca9c
Chandana Shankaregowda <chandana.shankaregowda@thoughtfocus.com>
2021-12-29 10:50:52 +0530

    add

--- Fetch/fetch.js
+++ Fetch/fetch.js
@@ -1,10 +1,16 @@
-let url= 'https://jsonplaceholder.typicode.com/posts'
-
-async function fetchPosts(){
-    let response = await fetch(url)
-    let data = await response.json();
-
+let url='https://jsonplaceholder.typicode.com/posts'
+async function fetchUrl(){
+    let response=await fetch(url)
+    let data=await response.json()
     console.log(data);
+    appendData(data)
 }
-
-fetchPosts()
+function appendData(data) {
+    var mainContainer = document.getElementById("container");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.innerHTML = `Title ${i+1}: ` + data[i].title;
+      mainContainer.appendChild(div);
+    }
+  }
+fetchUrl();
